c57043ce90e874356a0ea9b74d8ae3d4a8f15b12
anakin87 <stefanofiorucci@gmail.com>
2023-12-23 13:20:49 +0100

    .github/

--- scripts/create_readme.py
+++ scripts/create_readme.py
@@ -1,6 +1,6 @@
 import csv
 
-markdown_str="""
+markdown_str = """
 # My content collection 🎮📚
 
 A simple (and incomplete) collection of content I have created and shared over time.
@@ -8,12 +8,12 @@ A simple (and incomplete) collection of content I have created and shared over t
 | Title  | ≈ Date | type |
 |---|---|---|
 """
-        
-with open('./data/data.csv', 'r') as f:
-    reader = csv.reader(f, delimiter=',', quotechar='"')
+
+with open("./data/data.csv", "r") as f:
+    reader = csv.reader(f, delimiter=",", quotechar='"')
     for row in reader:
         title, url, date, type_ = row
-        markdown_str+= f'| [{title}]({url}) | {date} | {type_} |\n'
+        markdown_str += f"| [{title}]({url}) | {date} | {type_} |\n"
 
-with open('./README.md', 'w') as fo:
+with open("./README.md", "w") as fo:
     fo.write(markdown_str)
--- scripts/update_profile.py
+++ scripts/update_profile.py
@@ -1,19 +1,25 @@
 import csv
 import re
 
-markdown_str="<!--content start-->\n"       
-with open('data/data.csv', 'r') as f:
-    reader = csv.reader(f, delimiter=',', quotechar='"')
-    for i,row in enumerate(reader):
+markdown_str = "<!--content start-->\n"
+with open("data/data.csv", "r") as f:
+    reader = csv.reader(f, delimiter=",", quotechar='"')
+    for i, row in enumerate(reader):
         title, url, date, type_ = row
-        markdown_str+= f'- [{title} ({type_})]({url})\n'
-        if i==4:
+        markdown_str += f"- [{title} ({type_})]({url})\n"
+        if i == 4:
             break
-markdown_str+="<!--content end-->"
+markdown_str += "<!--content end-->"
 
-with open('profile/README.md', 'r') as fin:
+with open("profile/README.md", "r") as fin:
     profile_readme = fin.read()
 
-profile_readme = re.sub(r'<!--content start-->.*<!--content end-->', markdown_str, profile_readme, flags=re.DOTALL)
+profile_readme = re.sub(
+    r"<!--content start-->.*<!--content end-->",
+    markdown_str,
+    profile_readme,
+    flags=re.DOTALL,
+)
 
-print(profile_readme)
+with open("profile/README.md", "w") as fout:
+    fout.write(profile_readme)
