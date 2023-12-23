42c28c72bf34d6a1cb5563b95e64ea940dcd6256
anakin87 <stefanofiorucci@gmail.com>
2023-12-23 12:59:14 +0100

    add limit to csvreader

--- scripts/update_profile.py
+++ scripts/update_profile.py
@@ -4,9 +4,11 @@ import re
 markdown_str="<!--content start-->\n"       
 with open('data/data.csv', 'r') as f:
     reader = csv.reader(f, delimiter=',', quotechar='"')
-    for row in reader:
+    for i,row in enumerate(reader):
         title, url, date, type_ = row
         markdown_str+= f'- [{title} ({type_})]({url})\n'
+        if i==4:
+            break
 markdown_str+="<!--content end-->"
 
 with open('profile/README.md', 'r') as fin:
